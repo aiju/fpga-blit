9eb0422f4da6e37302697ddffb36bc2d14f7b9bc
Julius Schmidt <aiju@phicode.de>
2019-12-24 17:37:23 +0000

    combine fx68k source code into single file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-verilator $* -CFLAGS "`sdl2-config --cflags`" -LDFLAGS "`sdl2-config --libs`" --cc blit.v fx68k/*.sv -Wno-fatal --exe sim.cc && make -j -C obj_dir -f Vtop.mk && obj_dir/Vtop
+verilator $* -CFLAGS "`sdl2-config --cflags`" -LDFLAGS "`sdl2-config --libs`" --cc blit.v -Wno-fatal --exe sim.cc && make -j -C obj_dir -f Vtop.mk && obj_dir/Vtop
